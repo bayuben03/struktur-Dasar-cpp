3b8ce7f4ba6f375037871d8c3b1cbc49a4cf6bfd
Bayuben03 <188961165+bayuben03@users.noreply.github.com>
2024-11-19 15:32:15 +0700

    Add files via upload

---
+++ struktur.cpp
@@ -0,0 +1,22 @@
+// Belajar 1 - Struktur Dasar cpp
+// dalam cpp terdapat tipe data, variable, konstanta
+// dalam cpp juga terdapat Operator Aritmatika dan Logika
+
+#include <iostream>
+using namespace std;
+
+int main () {
+  // inisialisasi
+  int a;
+  int b;
+
+  // algoritma
+  a = 10;
+  b = 5;
+  a = a + b;
+  b = b - a;
+  cout << a << endl;
+  cout << b << endl;
+
+  return 0;
+}
